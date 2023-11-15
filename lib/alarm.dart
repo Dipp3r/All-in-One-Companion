bdf18dd5419da8966d2c7241061120637ec546a3
Dipp3r <aruneesh4@gmail.com>
2023-11-16 01:50:03 +0530

    created alarm page

---
+++ lib/alarm.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class Alarm extends StatefulWidget {
+  const Alarm({super.key});
+
+  @override
+  State<Alarm> createState() => _AlarmState();
+}
+
+class _AlarmState extends State<Alarm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){
+          Navigator.of(context).pop();
+        }, icon: const Icon(Icons.arrow_back_ios)),
+        title: const Text('Wakie-Wakie'),
+      ),
+    );
+  }
+}
